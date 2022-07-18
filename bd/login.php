45f16fc616da7e679f430f7247aad19b4e0b2b6a
kblackmc <kevin9714cast@gmail.com>
2022-07-18 02:20:44 -0500

    bd
    bd

---
+++ bd/login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+
+include_once 'conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+//recepción de datos enviados mediante POST desde ajax
+$usuario = (isset($_POST['usuario'])) ? $_POST['usuario'] : '';
+$password = (isset($_POST['password'])) ? $_POST['password'] : '';
+$tipo = (isset($_POST['tipo'])) ? $_POST['tipo'] : '';
+$email = (isset($_POST['email'])) ? $_POST['email'] : '';
+
+$pass = md5($password); //deseencripto la clave enviada por el usuario para compararla con la clava encriptada y almacenada en la BD
+$consulta = "SELECT * FROM usuarios  WHERE usuario='$usuario' AND password='$pass' AND tipo='$tipo'AND estado='Activo'";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+
+if($resultado->rowCount() >= 1){
+    $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+    $_SESSION["s_usuario"] = $usuario;
+    $_SESSION["s_tipo"] = $tipo;
+    $_SESSION["s_email"] = $email;
+    $_SESSION["s_pass"] =  $password;
+}else{
+    $_SESSION["s_usuario"] = null;
+    $data=null;
+}
+
+print json_encode($data);
+$conexion=null;
+
+//usuarios de pruebaen la base de datos
+//usuario:admin pass:12345
+//usuario:demo pass:demo
